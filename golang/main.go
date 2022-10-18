8b88f3d5266a575f3bebcd72afb92abf7e2298f2
Anastasia <progonskaij@icloud.com>
2022-10-18 12:54:58 +0300

    add math problem resolver

--- golang/main.go
+++ golang/main.go
@@ -9,39 +9,33 @@ func Log(base float64, num float64) float64 {
 	return math.Log(num) / math.Log(base)
 }
 
-func problem(x float64) float64 {
-	var a float64 = 4.1
-	var b float64 = 2.7
-	var numerator float64 = a*math.Pow(x, 1/3) - b*Log(5, x)
+func problem(x float64, a float64, b float64) float64 {
+	var numerator float64 = a*math.Pow(x, 1/3.0) - b*Log(5, x)
 	var denominator float64 = math.Pow(math.Log10(x-1), 3)
 	return numerator / denominator
 }
 
-func taskA() []float64 {
-	var x_start float64 = 1.5
-	var x_end float64 = 3.5
-	var delta float64 = 0.4
+func taskA(x_start float64, x_end float64, delta float64) []float64 {
 	var valuesCount int = int(math.Floor((x_end - x_start) / delta))
 	var values = make([]float64, valuesCount)
 
 	for i := 0; i < valuesCount; i++ {
-		values[i] = problem(x_start + float64(i)*delta)
+		values[i] = problem(x_start+float64(i)*delta, 4.1, 2.7)
 	}
 
 	return values
 }
 
-func taskB() [5]float64 {
-	var nums = [...]float64{1.9, 2.15, 2.34, 2.74, 3.16}
-	var values [5]float64
+func taskB(args ...float64) []float64 {
+	var values = make([]float64, len(args))
 
-	for i := 0; i < len(nums); i++ {
-		values[i] = problem(nums[i])
+	for i := 0; i < len(args); i++ {
+		values[i] = problem(args[i], 4.1, 2.7)
 	}
 	return values
 }
 
 func main() {
-	fmt.Println("задание А", taskA())
-	fmt.Println("Задание Б", taskB())
+	fmt.Println("задание А", taskA(1.5, 3.5, 0.4))
+	fmt.Println("Задание Б", taskB(1.9, 2.15, 2.34, 2.74, 3.16))
 }
---
+++ hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// this is a comment asdfasd
+
+func main() {
+    fmt.Println("Hello World")
+}
