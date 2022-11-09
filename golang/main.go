416c648f38cf83c646c6a4ca8039d64c7c8eeac4
kagam1i <evb1404@gmail.com>
2022-11-09 20:10:30 +0300

    Лабораторная работа №3 Балясова Елена 1/147 вариант №3

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func f(x float64) float64 {
+	var a float64 = 2.0
+	var b float64 = 0.95
+	var y float64 = (1 + math.Pow(math.Log10(a/b), 2.0)) / (b - math.Pow(2.718, x/a))
+	return y
+}
 
 func main() {
-	fmt.Println("Hello world")
+	// Задача А
+	fmt.Println("Задача А")
+
+	for x := 1.25; x <= 2.75; x += 0.3 {
+		fmt.Println("y(", x, ") =", f(x))
+	}
+
+	// Задача Б
+	fmt.Println("\nЗадача Б")
+	fmt.Println(f(2.2))
+	fmt.Println(f(3.78))
+	fmt.Println(f(4.51))
+	fmt.Println(f(6.58))
+	fmt.Println(f(1.2))
+
 }
