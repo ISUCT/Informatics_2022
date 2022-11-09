cf1a9ec2f953090931710698ebcc7a1a12cce214
Kraiben <stepan_belov_2016@mail.ru>
2022-11-09 20:16:06 +0300

    reinstall git and redact Count Lab

--- Count LAB/Program.cs
+++ Count LAB/Program.cs
@@ -7,18 +7,17 @@ namespace Informatics_2022
         static void Main()
         {
             Console.Write("Введите, ответ на какую задачу хотите получить (A или B):");
-            string zadacha = Console.ReadLine();
+            string? zadacha = Console.ReadLine();
             switch (zadacha)
             {
                 case "A":
-                    double[] x_massA = new double[] { 2.5f, 1.28f, 3.28f, 0.4f };
-                    foreach (double x in x_massA)
+                    for (double x = 1.28 ; x <= 3.28; x+=0.4) 
                     {
                         double b_cube = Math.Pow(2.5f, 3);
                         double x_cube = Math.Pow(x, 3);
-                        double jopa1 = Math.Pow(Math.Sin(b_cube + x_cube), 2);
-                        double jopa2 = 1 + Math.Pow((x_cube + b_cube), 1 / 3);
-                        Console.WriteLine(jopa1 / jopa2);
+                        double verx = Math.Pow(Math.Sin(b_cube + x_cube), 2);
+                        double niz = 1 + Math.Pow((x_cube + b_cube), 1 / 3);
+                        Console.WriteLine(verx / niz);
                     }
                     Console.ReadLine();
                     break;
@@ -28,9 +27,9 @@ namespace Informatics_2022
                     {
                         double b_cube = Math.Pow(2.5f, 3);
                         double x_cube = Math.Pow(x, 3);
-                        double jopa1 = Math.Pow(Math.Sin(b_cube + x_cube), 2);
-                        double jopa2 = 1 + Math.Pow((x_cube + b_cube), 1 / 3);
-                        Console.WriteLine(jopa1 / jopa2);
+                        double verx = Math.Pow(Math.Sin(b_cube + x_cube), 2);
+                        double niz = 1 + Math.Pow((x_cube + b_cube), 1 / 3);
+                        Console.WriteLine(verx / niz);
                     }
                     Console.ReadLine();
                     break;
