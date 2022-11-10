429c74a005c8291526fd80b9aea35fb9123cd1d2
AlyonaUi <d4nilovaaliona@yandex.ru>
2022-11-10 19:03:23 +0300

    Create main.go

---
+++ golang/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	for x := 0.7; x <= 2.2; x += 0.3 {
+		a := 1.2
+		b := 0.48
+		const e float64 = 2.7182
+		var y float64 = (math.Pow(b, 3) + math.Pow(math.Sin(a*x), 2)) / (math.Acos(x*b*x) + (math.Pow(e, -x/2)))
+		fmt.Println(y)
+	}
+}
