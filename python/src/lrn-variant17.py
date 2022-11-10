99c2b0d4871dd40b0b044bdb235396f5fba801e1
WIN-DNSN7V4BNCF\Александр <a.ozerski2@mail.ru>
2022-11-10 22:58:32 +0300

    LRN-3-update-1.3

--- python/src/lrn-variant17.py
+++ python/src/lrn-variant17.py
@@ -4,29 +4,27 @@ a = 0.1
 b = 0.5
 x = 0.15
 
-print ( '  ' ) 
-print ( ' Задание А:' ) 
-print ( '  ' ) 
+print('  ')
+print('Задание А:')
+print('  ') 
 
 while x <= 1.37:
-    print ( ( a + ( math.tan (b * x) ) ** 2 ) / ( b + ( 1 / math.tan ( a * x ) ) ** 2 ) ) 
+    print(( a + (math.tan (b * x)) ** 2) / (b + (1 / math.tan (a * x)) ** 2))
     x += 0.25
 
-print ( '  ' ) 
-print ( 'Задание B:' ) 
-print ( '  ' ) 
+print('  ')
+print('Задание B:')
+print('  ')
 
 x = 0.2
-print ( ( a + ( math.tan (b * x) ) ** 2 ) / ( b + ( 1 / math.tan ( a * x ) ) ** 2 ) ) 
+print(( a + (math.tan (b * x)) ** 2) / (b + (1 / math.tan (a * x)) ** 2))
 
 x = 0.3
-print ( ( a + ( math.tan (b * x) ) ** 2 ) / ( b + ( 1 / math.tan ( a * x ) ) ** 2 ) ) 
-
+print(( a + (math.tan (b * x)) ** 2) / (b + (1 / math.tan (a * x)) ** 2))
 x = 0.44
-print ( ( a + ( math.tan (b * x) ) ** 2 ) / ( b + ( 1 / math.tan ( a * x ) ) ** 2 ) ) 
+print(( a + (math.tan (b * x)) ** 2) / (b + (1 / math.tan (a * x)) ** 2))
 
 x = 0.6
-print ( ( a + ( math.tan (b * x) ) ** 2 ) / ( b + ( 1 / math.tan ( a * x ) ) ** 2 ) ) 
-
+print(( a + (math.tan (b * x)) ** 2) / (b + (1 / math.tan (a * x)) ** 2))
 x = 0.56
-print ( ( a + ( math.tan (b * x) ) ** 2 ) / ( b + ( 1 / math.tan ( a * x ) ) ** 2 ) ) 
+print(( a + (math.tan (b * x)) ** 2) / (b + (1 / math.tan (a * x)) ** 2))
