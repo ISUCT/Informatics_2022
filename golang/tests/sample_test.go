952b49f167721d770efc26f1be6ff472eeac7d49
RedMarshal <redmarshal37@gmail.com>
2022-11-11 20:12:01 +0300

    Remove tests

--- golang/tests/sample_test.go
+++ golang/tests/sample_test.go
@@ -1,15 +1,1 @@
 package internal_test
-
-import (
-	"testing"
-
-	"isuct.ru/informatics2022/internal"
-)
-
-func TestLog(t *testing.T) {
-	result := internal.Log(100, 10)
-
-	if result != 0.5 {
-		t.Fatalf(`Summ(10,100) = %.4f, want 0.5, error`, result)
-	}
-}
