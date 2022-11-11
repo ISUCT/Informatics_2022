914b56c0cbdfccaf3ca7f2d347268d83ca95b59c
sergeykochiev <sergeyivanovo547@gmail.com>
2022-11-12 00:56:20 +0300

    lab

--- python/src/main.py
+++ python/src/main.py
@@ -11,13 +11,13 @@ import math
 
 # задача 1
 print('Задача 1:')
-x=0.11
-while x<=0.36:
-    print(math.asin(x**2)+math.acos(x**3))
-    x+=0.05
+x = 0.11
+while x <= 0.36:
+    print(math.asin(x ** 2) + math.acos(x ** 3))
+    x += 0.05
 
 # задача 2
 print('Задача 2:')
-x2=[0.08,0.26,0.35,0.41,0.53]
+x2 = [0.08, 0.26, 0.35, 0.41, 0.53]
 for n in range (len(x2)):
-    print(math.asin(x2[n]**2)+math.acos(x2[n]**3))
+    print(math.asin(x2[n] ** 2) + math.acos(x2[n] ** 3))
