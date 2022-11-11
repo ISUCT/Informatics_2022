e828edeba60658466cf00a282fa7d142aa951c43
sergeykochiev <sergeyivanovo547@gmail.com>
2022-11-12 00:43:07 +0300

    lab functions

--- python/src/main.py
+++ python/src/main.py
@@ -1,7 +1,23 @@
-def summ(a: int, b: int) -> int:
-    return a + b
+#def summ(a: int, b: int) -> int:
+#    return a + b
 
 
-if __name__ == "__main__":
-    print("Hello world")
-    print(summ(3, 4))
+#if __name__ == "__main__":
+#    print("Hello world")
+#    print(summ(3, 4))
+
+# лабораторная работа на вычисление функций
+import math
+
+# задача 1
+print('Задача 1:')
+x=0.11
+while x<=0.36:
+    print(math.asin(x**2)+math.acos(x**3))
+    x+=0.05
+
+# задача 2
+print('Задача 2:')
+x2=[0.08,0.26,0.35,0.41,0.53]
+for n in range (len(x2)):
+    print(math.asin(x2[n]**2)+math.acos(x2[n]**3))
