ea61fbb1c3c78bd96be6621cb1978762eddf7973
Kraiben <stepan_belov_2016@mail.ru>
2022-11-11 11:58:53 +0300

    build Program.cs

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,24 +1,25 @@
-﻿using System;
-
-namespace Informatics_2022
+﻿namespace Informatics_2022
 {
-    class Count_LAB 
+    using System;
+
+    internal class Program
     {
-        static void Main()
-        {
+         private static void Main()
+         {
             Console.Write("Введите, ответ на какую задачу хотите получить (A или B):");
             string zadacha = Console.ReadLine();
             switch (zadacha)
             {
                 case "A":
-                    for (double x = 1.28 ; x <= 3.28; x+=0.4) 
+                    for (double x = 1.28; x <= 3.28; x += 0.4)
                     {
                         double b_cube = Math.Pow(2.5f, 3);
                         double x_cube = Math.Pow(x, 3);
                         double verx = Math.Pow(Math.Sin(b_cube + x_cube), 2);
-                        double niz = 1 + Math.Pow((x_cube + b_cube), 1 / 3);
+                        double niz = 1 + Math.Pow(x_cube + b_cube, 1 / 3);
                         Console.WriteLine(verx / niz);
                     }
+
                     Console.ReadLine();
                     break;
                 case "B":
@@ -28,12 +29,12 @@ namespace Informatics_2022
                         double b_cube = Math.Pow(2.5f, 3);
                         double x_cube = Math.Pow(x, 3);
                         double verx = Math.Pow(Math.Sin(b_cube + x_cube), 2);
-                        double niz = 1 + Math.Pow((x_cube + b_cube), 1 / 3);
+                        double niz = 1 + Math.Pow(x_cube + b_cube, 1 / 3);
                         Console.WriteLine(verx / niz);
                     }
+
                     Console.ReadLine();
                     break;
-
             }
         }
     }
