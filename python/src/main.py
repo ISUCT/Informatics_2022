867030c2455af5b9b2e2097df3667c187d389705
Loopeq <arsen.00z11@gmail.com>
2022-11-11 22:45:35 +0300

    laba_func

--- python/src/main.py
+++ python/src/main.py
@@ -9,7 +9,7 @@ def lab_var_13_1(a: float, b: float, x_start: float, x_end: float, step: float):
     y = None
     while x_start <= x_end:
         try:
-            y = (a * (x_start**0.5) - b * math.log(x_start, 5)) / (math.log10(abs(x_start-1)))
+            y = (a * (x_start ** 0.5) - b * math.log(x_start, 5)) / (math.log10(abs(x_start - 1)))
         except Exception as Error:
             print(Error, x_start)
         x_start += step
@@ -24,5 +24,5 @@ def lab_var_13_2(app: [], a: float, b: float):
 
 if __name__ == "__main__":
     lab_var_13_1(4.1, 2.7, 1.2, 5.2, 0.8)
-    print("-"*30)
+    print("-" * 30)
     lab_var_13_2([1.9, 2.15, 2.34, 2.73, 3.16], 4.1, 2.7)
