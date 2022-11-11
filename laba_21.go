37753bd7b487ca640ffc3b5dd2a36e292cb32d8b
EgoriusTheGreat2 <117865797+EgoriusTheGreat2@users.noreply.github.com>
2022-11-11 19:26:54 +0300

    Update laba_21.go

--- laba_21.go
+++ laba_21.go
@@ -8,11 +8,11 @@ import (
 func main() {
   fmt.Println("A: ")
   for x := 0.11; x <= 0.36; x += 0.05{
-    sin := math.Sin(x) 
-    cos := math.Cos(x)
+	sin := math.Sin(x) 
+	cos := math.Cos(x)
 	ln := math.Log(x) //натуральный логарифм x  
 	fin := (math.Pow(sin, 3) + math.Pow(cos, 3)) * ln //конечная формула
-    fmt.Println(fin)
+	fmt.Println(fin)
   }
 	//var x [5]float64 = [5]float64 {0.2, 0.3, 0.38, 0.43, 0.57}
 	//sin := math.Sin(x) 
