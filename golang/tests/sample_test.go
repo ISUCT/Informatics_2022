83178c59c40f0ade8fb66fd20ddf77d51fb6cf29
RedMarshal <redmarshal37@gmail.com>
2022-11-11 20:19:21 +0300

    fix python tests

--- golang/tests/sample_test.go
+++ golang/tests/sample_test.go
@@ -1,1 +1,14 @@
 package internal_test
+
+import (
+	"main"
+	"testing"
+)
+
+func TestLog(t *testing.T) {
+	result := main.Log(100, 10)
+
+	if result != 0.5 {
+		t.Fatalf(`Summ(10,100) = %.4f, want 0.5, error`, result)
+	}
+}
--- python/tests/test_main.py
+++ python/tests/test_main.py
@@ -2,35 +2,11 @@ import unittest
 from src import main
 
 
-class SummTests(unittest.TestCase):
+class LogTests(unittest.TestCase):
 
     def test_positive(self):
-        res = main.summ(2, 3)
-        self.assertEqual(5, res)
-
-    def test_zero(self):
-        res = main.summ(0, 0)
-        self.assertEqual(0, res)
-
-    def test_one_negative(self):
-        res = main.summ(-2, 3)
-        self.assertEqual(1, res)
-
-    def test_both_negative(self):
-        res = main.summ(-2, -4)
-        self.assertEqual(-6, res)
-
-    def test_one_negative_zero_res(self):
-        res = main.summ(-2, 2)
-        self.assertEqual(0, res)
-
-    def test_one_negative_and_text(self):
-        try:
-            main.summ(-2, "2")
-        except:
-            self.assertTrue(True)
-            return
-        self.fail()
+        res = main.Log(100, 10)
+        self.assertEqual(0.5, res)
 
 
 if __name__ == '__main__':
