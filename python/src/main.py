336d3e3af5b116bcabced8a8d167548f1b5f9e45
Enc0re232 <118062627+Enc0re232@users.noreply.github.com>
2022-11-12 18:07:17 +0300

    lab 1

--- python/src/main.py
+++ python/src/main.py
@@ -1,7 +1,30 @@
-def summ(a: int, b: int) -> int:
-    return a + b
+import math
+
+a = 4.1
+b = 2.7
+
+def Y(x):
+    global a, b
+    
+    return (a * (x ** 1/3) - b * math.log(x, 5)) / (math.log(x - 1, 10) ** 3)
+    
+
+#  task №23 A
+print("task №23 A")
+
+x_first = 1.5
+x_last = 3.5
+step = 0.4
+
+x = x_first
+while x < x_last:
+    print(f"y({x}) = {Y(x)}")
+    x+=step
 
 
-if __name__ == "__main__":
-    print("Hello world")
-    print(summ(3, 4))
+#  task №23 B
+print("task №23 B")
+
+array_x = [1.9, 2.15, 2.34, 2.74, 3.16]
+for x in array_x:
+    print(f"y({x}) = {Y(x)}")
