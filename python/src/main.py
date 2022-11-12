c3aa00efec4480174dcfbfd049f97cf0d97ef798
Stepan Lebedev <115336305+SOX05@users.noreply.github.com>
2022-11-12 11:02:16 +0300

    commit

--- python/src/main.py
+++ python/src/main.py
@@ -1,7 +1,22 @@
-def summ(a: int, b: int) -> int:
-    return a + b
+import math
+def  frange(start,stop,step):
+
+    i=start
+    while i < stop:
+        yield i 
+        i += step
+def y(x):
+    y=(a**(x**2)-1)-math.log10((x**2)-1)+pow((x**2)-1,1/3)
+    return y
+for x in frange (1.2,3.7,0.5):
+    a =1.6
+    print(y(x))
 
 
-if __name__ == "__main__":
-    print("Hello world")
-    print(summ(3, 4))
+
+xrange=[1.28,1.36,2.47,3.68,4.56]
+xlen =len(xrange)
+for i in range(0,xlen):
+    x=xrange[i]
+    a = 1.6
+    print(y(x))
