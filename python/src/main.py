b30ba2b07ba212fa1170b9a509ad724fe74fd022
SergeyAnisimov <115336230+SeregeyAnisimov@users.noreply.github.com>
2022-11-12 08:45:40 +0300

    Update main.py

--- python/src/main.py
+++ python/src/main.py
@@ -8,30 +8,21 @@ if __name__ == "__main__":
     print("Hello world")
     print(summ(3, 4))
 
-#A
 
 x = float(1.14)
 a = float(1.35)
 b = float(0.98)
 while 1.14 <= x <= 4.24:
-    z = (math.log(x,10))**2
-    y = ((a*x + b)**(1/3))/z
+    z = (math.log(x, 10))**2
+    y = (( a * x + b) ** ( 1 / 3 ) ) / z
     x += 0.62
     print('A) ', y)
 
-#B
-n = [0.35,1.28,3.51,5.21,4.16]
+n = [0.35, 1.28, 3.51, 5.21, 4.16]
 a = float(1.35)
 b = float(0.98)
 for i in range(len(n)):
     x = n[i]
     z = (math.log(x,10))**2
-    y = ((a*x + b)**(1/3))/z
-    print('B) ',y)
-
-
-
-
-
-
-
+    y = (( a * x + b) ** ( 1 / 3 ) ) / z
+    print('B) ', y)
