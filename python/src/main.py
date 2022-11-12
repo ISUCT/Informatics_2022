674fa485579c54ae53495c46a3cffed71fc25b79
Mewsha <adam.corvum@yandex.ru>
2022-11-12 10:53:22 +0300

    Update main.py

--- python/src/main.py
+++ python/src/main.py
@@ -4,23 +4,25 @@ import math
 def lab_var_22_1(a: float, x_s: float, x_e: float, step: float):
     while x_s <= x_e:
         try:
-            y = a**(x_s**2 - 1) - math.log((x_s**2 - 1), 10) + (x_s**2 - 1)**(1/3)
+            y = a ** (x_s ** 2 - 1) - math.log((x_s ** 2 - 1), 10) + (x_s ** 2 - 1) ** (1 / 3)
         except Exception as Error:
             print(Error, x_s)
         x_s += step
         print(y)
 
-def lab_var_22_2(app: [], a: float):
-     for num in app:
-         y = a ** (num ** 2 - 1) - math.log((num ** 2 - 1), 10) + (num ** 2 - 1) ** (1 / 3)
-         print(y)
 
-if __name__ == "__main__":
+def lab_var_22_2(app: [], a: float):
+    for num in app:
+        y = a ** (num ** 2 - 1) - math.log((num ** 2 - 1), 10) + (num ** 2 - 1) ** (1 / 3)
+        print(y)
+
+
+if "__main__" != __name__:
+    pass
+else:
     print('zadanie 1')
     lab_var_22_1(2.25, 1.2, 2.7, 0.3)
 
     print('zadanie 2')
 
     lab_var_22_2([4.48, 3.56, 2.78, 5.28, 3.21], 2.25)
-
-
