1f59cf1073d676083357132f10b054edcc384ac0
VHarndy <117998993+VHarndy@users.noreply.github.com>
2022-11-12 08:53:30 +0300

    add labwork var24

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+//Lab 24 Chukhlova V 1.42
 
 func main() {
-	fmt.Println("Hello world")
+	var a = 7.2
+	var b = 1.3
+	fmt.Println("Задание А")
+	for x := 1.56; x <= 4.71; x += 0.63 {
+		c := (a + (b * x)) / (math.Pow(math.Log10(x), 3))
+		y := math.Pow(c, (1 / 5.0))
+		fmt.Println("x = ", x, " ", "y = ", y)
+	}
+	var mass = [5]float64{2.4, 2.8, 3.9, 4.7, 3.16}
+	fmt.Println("Задание B")
+	for _, x1 := range mass {
+		c := (a + (b * x1)) / (math.Pow(math.Log10(x1), 3))
+		y1 := math.Pow(c, (1 / 5.0))
+		fmt.Println("x = ", x1, " ", "y = ", y1)
+	}
 }
