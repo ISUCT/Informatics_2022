614c4450fef212f64a6dd01213b430d401d3369c
sergeykochiev <sergeyivanovo547@gmail.com>
2022-11-12 09:36:34 +0300

    fised tests

--- python/src/main.py
+++ python/src/main.py
@@ -1,10 +1,10 @@
-# def summ(a: int, b: int) -> int:
-#   return a + b
+def summ(a: int, b: int) -> int:
+    return a + b
 
 
-# if __name__ == "__main__":
-#   print("Hello world")
-#   print(summ(3, 4))
+if __name__ == "__main__":
+    print("Hello world")
+    print(summ(3, 4))
 
 # лабораторная работа на вычисление функций
 import math
