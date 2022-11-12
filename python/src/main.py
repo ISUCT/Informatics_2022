8da60379af4d5735a54ab6c9ed47169c61e72e2e
Loopeq <arsen.00z11@gmail.com>
2022-11-12 10:49:54 +0300

    123

--- python/src/main.py
+++ python/src/main.py
@@ -1,4 +1,5 @@
-import laba3
+from src.laba3 import lab_var_13_2
+from src.laba3 import lab_var_13_1
 
 
 def summ(a: int, b: int) -> int:
@@ -6,5 +7,5 @@ def summ(a: int, b: int) -> int:
 
 
 if __name__ == "__main__":
-    laba3.lab_var_13_1(4.1, 2.7, 1.2, 5.2, 0.8)
-    laba3.lab_var_13_2([1.9, 2.15, 2.34, 2.73, 3.16], 4.1, 2.7)
+    lab_var_13_1(4.1, 2.7, 1.2, 5.2, 0.8)
+    lab_var_13_2([1.9, 2.15, 2.34, 2.73, 3.16], 4.1, 2.7)
