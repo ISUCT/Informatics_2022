c71ffb9abfa546310f8940a3762a4a4ed159c508
TyutinaAnastasia <nasty.tyutina@yandex.ru>
2022-11-13 16:05:32 +0300

    lab 19

--- python/src/main.py
+++ python/src/main.py
@@ -1,7 +1,27 @@
-def summ(a: int, b: int) -> int:
-    return a + b
+import math
+
+i = 0
 
 
-if __name__ == "__main__":
-    print("Hello world")
-    print(summ(3, 4))
+def frange(start, stop, step):
+    i = start
+    while i < stop:
+        yield i
+        i += step
+
+
+def y(x):
+    y = ((math.log(a + x)) / ((a + x) ** 2))
+    return y
+
+
+for x in frange(1.2, 4.2, 0.6):
+    a = 2.0
+    print(y(x))
+print()
+xrange = [1.16, 1.32, 1.47, 1.65, 1.93]
+xlen = len(xrange)
+for i in range(0, xlen):
+    x = xrange[i]
+    a = 2.0
+    print(y(x))
