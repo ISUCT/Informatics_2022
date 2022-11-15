b4f27fe34aeb4a843de45e66c9d6b13ba5d69f02
Fulfick <stepan_belov_2016@mail.ru>
2022-11-15 12:58:44 +0300

    add void for Lab

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,8 +1,9 @@
 ﻿namespace Informatics_2022
 {
     using System;
+    using CourseApp;
 
-    internal class Program
+    internal class Program : Task
     {
          private static void Main()
          {
@@ -13,24 +14,17 @@
                 case "A":
                     for (double x = 1.28; x <= 3.28; x += 0.4)
                     {
-                        double b_cube = Math.Pow(2.5f, 3);
-                        double x_cube = Math.Pow(x, 3);
-                        double verx = Math.Pow(Math.Sin(b_cube + x_cube), 2);
-                        double niz = 1 + Math.Pow(x_cube + b_cube, 1 / 3);
-                        Console.WriteLine(verx / niz);
+                        Task.Alg(x);
                     }
 
                     Console.ReadLine();
                     break;
+
                 case "B":
                     double[] x_massB = new double[] { 1.1f, 2.4f, 3.6f, 1.7f, 3.9f };
                     foreach (double x in x_massB)
                     {
-                        double b_cube = Math.Pow(2.5f, 3);
-                        double x_cube = Math.Pow(x, 3);
-                        double verx = Math.Pow(Math.Sin(b_cube + x_cube), 2);
-                        double niz = 1 + Math.Pow(x_cube + b_cube, 1 / 3);
-                        Console.WriteLine(verx / niz);
+                        Task.Alg(x);
                     }
 
                     Console.ReadLine();
