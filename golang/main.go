79bee91ac54c215347c183ef4d1d7a5e72364491
Danila Efremov <daniel.efrem@yandex.ru>
2022-11-15 17:21:40 +0300

    laba var8

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Задание А")
+	var x float64 = 0.7
+	var xk float64 = 2.2
+	var a float64 = 1.2
+	var b float64 = 0.48
+	for ; x < xk; x = x + 0.3 {
+		var y float64 = (((math.Pow(b, 3)) + math.Pow(math.Sin(a*x), 2)) / ((math.Acos(x * b * x)) + (math.Pow(2.7183, -x/2))))
+		fmt.Println(y)
+	}
+	fmt.Println("Задание Б")
+	var numbersx [5]float64 = [5]float64{0.25, 0.36, 0.56, 0.94, 1.28}
+	var c int64 = 0
+	for ; c <= 4; c = c + 1 {
+		var y float64 = (((math.Pow(b, 3)) + math.Pow(math.Sin(a*numbersx[c]), 2)) / ((math.Acos(numbersx[c] * b * numbersx[c])) + (math.Pow(2.7183, -numbersx[c]/2))))
+		fmt.Println(y)
+
+	}
 }
