c7197b9a1abb6bb5a74ed812aad319c60f3e7704
Mary2811 <ivanushkaaa2112@gmail.com>
2022-11-15 22:05:00 +0300

    лаб1 с введением функции

--- golang/main.go
+++ golang/main.go
@@ -5,20 +5,20 @@ import (
 	"math"
 )
 
+func function(a, x float64) float64 {
+	var y float64 = math.Pow(a, (math.Pow(x, 2)-1)) - math.Log10(math.Pow(x, 2)-1) + math.Pow((math.Pow(x, 2)-1), (1.0/3.0))
+	return y
+}
+
 func main() {
 	var a = 1.6
 	fmt.Println("Задание А")
 	for x := 1.2; x <= 3.7; x += 0.5 {
-		b := math.Pow(x, 2) - 1
-		y := math.Pow(a, b) - math.Log10(b) + math.Pow(b, (1/3))
-
-		fmt.Println("x = ", x, " ", "y = ", y)
+		fmt.Println("x = ", x, " ", "y = ", function(a, x))
 	}
 	var znach = [5]float64{1.28, 1.36, 2.47, 3.68, 4.56}
 	fmt.Println("Задание B")
 	for _, x1 := range znach {
-		b := math.Pow(x1, 2) - 1
-		y1 := math.Pow(a, b) - math.Log10(b) + math.Pow(b, (1/3))
-		fmt.Println("x = ", x1, " ", "y = ", y1)
+		fmt.Println("x = ", x1, " ", "y = ", function(a, x1))
 	}
 }
