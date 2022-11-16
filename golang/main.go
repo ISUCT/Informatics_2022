cd0868205e9c2035f2fefcbcfe0059acffe4241d
fagirton <fagirton2015@gmail.com>
2022-11-16 18:02:03 +0300

    Переменные А и Б теперь в main, задания А и Б разделены по функциям

--- golang/main.go
+++ golang/main.go
@@ -6,22 +6,34 @@ import (
 )
 
 func main() {
-	fmt.Println("Задание А")
-	var i float64 = 1.5
-	for ; i < 3.5; i = i + 0.4 {
-		fmt.Println(formula(i))
-	}
-	fmt.Println("Задание В")
-	fmt.Println(formula(1.9))
-	fmt.Println(formula(2.15))
-	fmt.Println(formula(2.34))
-	fmt.Println(formula(2.74))
-	fmt.Println(formula(3.16))
+	const a float64 = 4.1
+	const b float64 = 2.7
+
+	fmt.Println(taskA(a, b))
+	fmt.Println(taskB(a, b))
 }
 
-func formula(x float64) float64 {
-	var a float64 = 4.1
-	var b float64 = 2.7
+func formula(x float64, a float64, b float64) float64 {
 	var y float64 = ((a*a*a)*(math.Cbrt(x)) - b*(math.Log10(x)/math.Log10(5))) / (math.Pow(math.Log10(x-1), 3))
 	return y
+}
+
+func taskA(a float64, b float64) []float64 {
+	fmt.Println("Задание А")
+	var i float64 = 1.5
+	var res []float64
+	for ; i < 3.5; i = i + 0.4 {
+		res = append(res, formula(i, a, b))
+	}
+	return res
+}
+func taskB(a float64, b float64) []float64 {
+	fmt.Println("Задание B")
+	var x []float64 = []float64{1.9, 2.15, 2.34, 2.74, 3.16}
+	var res []float64
+	var i int8
+	for ; i < 5; i++ {
+		res = append(res, formula(x[i], a, b))
+	}
+	return res
 }
