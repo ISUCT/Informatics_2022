8a453276c6cf09ac62506e32752e64546a7e8650
Rewqaf41 <danyburdin@gmail.com>
2022-11-16 19:47:55 +0300

    update lab.py(add if name == main)

--- python/src/lab.py
+++ python/src/lab.py
@@ -4,6 +4,7 @@ a = 2.0
 b = 0.95
 x = 1.25
 print('Задание А:')
+# Задание А
 
 
 def y(x):
@@ -11,18 +12,15 @@ def y(x):
     return y
 
 
-while x < 2.75:
-    print(y(x))
-    x += 0.3
+if __name__ == '__main__':
+    while x < 2.75:
+        print(y(x))
+        x += 0.3
 
-
-# for x in task_a(1.25, 2.75, 0.3):
-#     print(y(x))
-
-
-print('Задание B:')
-xlist = [2.2, 3.78, 4.51, 6.58, 1.2]
-xlen = len(xlist)
-for i in range(0, xlen):
-    x = xlist[i]
-    print(y(x))
+    # Задание B
+    print('Задание B:')
+    xlist = [2.2, 3.78, 4.51, 6.58, 1.2]
+    xlen = len(xlist)
+    for i in range(0, xlen):
+        x = xlist[i]
+        print(y(x))
