96138a7caa9d143f5506f1227a8de7c0ff5c1097
Nohop09 <31438332+Nohop09@users.noreply.github.com>
2022-11-16 21:29:28 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -11,7 +11,6 @@ func main() {
 	fmt.Println("Mass")
 	digit := [5]float64{4.48, 3.56, 2.78, 5.28, 3.21}
 	Mass(0.4, 0.8, digit)
-
 }
 func lab(a float64, b float64, x float64) float64 {
 	return (((math.Pow(a, x) - math.Pow(b, x)) / math.Log10(a/b)) * math.Pow((a*b), 1.0/3.0))
