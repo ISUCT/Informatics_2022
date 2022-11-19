82f5e3665c2dc8ffa8c5c536515f2e404b3db662
Хализова Анна Романовна <117760288+HalizovaAnna@users.noreply.github.com>
2022-11-19 09:21:57 +0300

    Update main.py

--- python/src/main.py
+++ python/src/main.py
@@ -6,23 +6,28 @@ if __name__ == "__main__":
     print("Hello world")
     print(summ(3, 4))
 
-def frange(start,stop,step):
-    i=start
-    while i<stop:
-        yield i
-        i+=step
-def y(x):
-    y=((((x-a)**2)**(1.0/3))+(abs(x+b)**(1.0/5)))/((x**2-(a+b)**2)**(1.0/9))
-    return y
-for x in frange(1.23,7.23,1.2):
-    a=0.8
-    b=0.4
-    print(y(x))
 
-xrange=[1.88,2.26,3.84,4.55,-6.21]
-lenx=len(xrange)
-for i in range(0,lenx):
-    x=xrange[i]
-    a=0.8
-    b=0.4
+def frange(start, stop, step):
+    i = start
+    while i < stop:
+        yield i
+        i += step
+
+
+def y(x):
+    y = ((((x - a) ** 2) ** (1.0 / 3)) + (abs(x + b) ** (1.0 / 5))) / ((x ** 2 - (a + b) ** 2) ** (1.0 / 9))
+    return y
+
+
+for x in frange(1.23, 7.23, 1.2):
+    a = 0.8
+    b = 0.4
+    print(y(x))
+# B
+xrange = [1.88, 2.26, 3.84, 4.55, -6.21]
+lenx = len(xrange)
+for i in range(0, lenx):
+    x = xrange[i]
+    a = 0.8
+    b = 0.4
     print(y(x))
