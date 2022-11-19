6e1fd478d8d38478d9ccc5233d10943d19962b21
ArtemAlekseev2004 <117950938+ArtemAlekseev2004@users.noreply.github.com>
2022-11-19 08:52:22 +0300

    выразил функцию через функцию

--- python/src/main.py
+++ python/src/main.py
@@ -1,7 +1,14 @@
-def summ(a: int, b: int) -> int:
-    return a + b
-
-
 if __name__ == "__main__":
-    print("Hello world")
-    print(summ(3, 4))
+    import math
+def f(x):
+    b = 2.5
+    return (1 + (math.sin(b ** 3 + x ** 3)) ** 2) / ((b ** 3 + x ** 3) ** 1/3)
+xn = 1.28
+while xn <= 3.28:
+    print (f(xn))
+    xn += 0.4
+print (f(1.1))
+print (f(2.4))
+print (f(3.6))
+print (f(1.7))
+print (f(3.9))
