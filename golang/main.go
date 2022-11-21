09e0ed5a289ab29a4372ae9b757fc40f62d3642d
SadovnichiyDenis <114995964+SadovnichiyDenis@users.noreply.github.com>
2022-11-21 13:45:43 +0300

    Исправления
    1. Добавил функции задач
    2. Добавил Ds_Store  в игнор
    3. Добавил константу а

--- golang/main.go
+++ golang/main.go
@@ -6,22 +6,32 @@ import (
 )
 
 func main() {
+	const a float64 = 2.0
 	fmt.Println("___Задание А___")
-	var x float64 = 1.2
-	for ; x < 4.2; x = x + 0.6 {
-		fmt.Println(formula(x))
-	}
+	fmt.Println(TaskA(a))
 	fmt.Println("___Задание В___")
-	fmt.Println(formula(1.16))
-	fmt.Println(formula(1.32))
-	fmt.Println(formula(1.47))
-	fmt.Println(formula(1.65))
-	fmt.Println(formula(1.93))
+	fmt.Println(TaskB(a))
 }
 
-func formula(x float64) float64 {
-	var a float64 = 2.0
+func TaskA(a float64) []float64 {
+	var x float64 = 1.2
+	var taskA []float64
+	for ; x < 4.2; x = x + 0.6 {
+		taskA = append(taskA, formula(x, a))
+	}
+	return taskA
+}
 
+func TaskB(a float64) []float64 {
+	var taskB []float64
+	var masB = []float64{1.16, 1.32, 1.47, 1.65, 1, 93}
+	for i := 0; i < len(masB); i++ {
+		taskB = append(taskB, formula(masB[i], a))
+	}
+	return taskB
+}
+
+func formula(x float64, a float64) float64 {
 	var y float64 = (math.Log10(a + x)) / ((a + x) * (a + x))
 	return y
 }
