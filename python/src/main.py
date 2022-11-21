a54e866ccc33cb02e9cd475f63cf9d19794014a3
DariShantalova <118109238+DariShantalova@users.noreply.github.com>
2022-11-21 16:09:04 +0300

    Update main.py

--- python/src/main.py
+++ python/src/main.py
@@ -1,3 +1,4 @@
+import math
 def summ(a: int, b: int) -> int:
     return a + b
 
@@ -5,3 +6,17 @@ def summ(a: int, b: int) -> int:
 if __name__ == "__main__":
     print("Hello world")
     print(summ(3, 4))
+
+
+
+x = 0.11
+while 0.11<= x <=0.36:
+    y= ((math.sin(x) ** 3) + (math.cos(x) ** 3)) * math.log10(x)
+    x += 0.05
+    print('A)', y)
+
+n= [0.2, 0.3, 0.38, 0.43, 0.57]
+for i in range(len(n)):
+    x=n[i]
+    y=((math.sin(x) ** 3) + (math.cos(x) ** 3)) * math.log10(x)
+    print('B)', y)
