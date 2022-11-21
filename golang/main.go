d3ac56428d088ef0e693170ff4b7d97cc683f775
GitSwaggCat <113989366+GitSwaggCat@users.noreply.github.com>
2022-11-21 16:55:56 +0300

    Лабораторная с разделением заданий вариант 16

--- golang/main.go
+++ golang/main.go
@@ -8,8 +8,8 @@ import (
 func main() {
 	const a float64 = 2.0
 	const b float64 = 1.1
-	TaskA(a, b)
-	fmt.Println(TaskB(a, b))
+	taskb(a, b)
+	fmt.Println(taskb(a, b))
 }
 
 func formula(x float64, a float64, b float64) float64 {
@@ -17,7 +17,7 @@ func formula(x float64, a float64, b float64) float64 {
 	return y
 }
 
-func TaskA(a float64, b float64) {
+func taska(a float64, b float64) {
 	fmt.Println("Задание А")
 	var x float64 = 0.08
 	for ; x < 1.08; x = x + 0.2 {
@@ -25,7 +25,7 @@ func TaskA(a float64, b float64) {
 	}
 }
 
-func TaskB(a float64, b float64) [5]float64 {
+func taskb(a float64, b float64) [5]float64 {
 	fmt.Println("Задание В")
 	var Massiv = [5]float64{formula(0.1, a, b), formula(0.3, a, b), formula(0.4, a, b), formula(0.45, a, b), formula(0.65, a, b)}
 	return Massiv
