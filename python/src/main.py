9d35178d5ffaaaadbcb777f1f455ff4c03093ba2
SergeyAnisimov <115336230+SeregeyAnisimov@users.noreply.github.com>
2022-11-22 18:20:14 +0300

    Update main and test

--- python/src/main.py
+++ python/src/main.py
@@ -10,20 +10,24 @@ if __name__ == "__main__":
     print(summ(3, 4))
 
 
-x = float(1.14)
-a = float(1.35)
-b = float(0.98)
-while 1.14 <= x <= 4.24:
-    z = (math.log(x, 10))**2
-    y = ((a * x + b) ** (1 / 3)) / z
-    x += 0.62
-    print('A) ', y)
+def Aformula(x: float = 2.1, a: float = 1.35, b: float = 0.98):
+    list_reply = []
+    if 1.14 > x or x > 4.24:
+        list_reply.append(None)
+    while 1.14 <= x <= 4.24:
+        x += 0.62
+        y = ((a * x + b) ** (1 / 3)) / ((math.log(x, 10))**2)
+        list_reply.append(y)
+    return list_reply
 
-n = [0.35, 1.28, 3.51, 5.21, 4.16]
-a = float(1.35)
-b = float(0.98)
-for i in range(len(n)):
-    x = n[i]
-    z = (math.log(x, 10))**2
-    y = ((a * x + b) ** (1 / 3)) / z
-    print('B) ', y)
+
+def Bformula(variables=[0.35, 1.28, 3.51, 5.21, 4.16], a=1.35, b=0.98):
+    list_reply = []
+    for i in range(len(variables)):
+        x = variables[i]
+        if x != 1 and x > 0:
+            y = ((a * x + b) ** (1 / 3)) / ((math.log(x, 10))**2)
+            list_reply.append(y)
+        else:
+            list_reply.append(None)
+    return list_reply
--- python/tests/test_main.py
+++ python/tests/test_main.py
@@ -33,5 +33,42 @@ class SummTests(unittest.TestCase):
         self.fail()
 
 
+class FormulaTestsA(unittest.TestCase):
+    def test_A(self):
+        res = main.Aformula(3.1)
+        self.assertEqual([5.5828418553790575, 4.670726993390387], res)
+
+    def test_A_zero(self):
+        res = main.Aformula(0)
+        self.assertEqual([None], res)
+
+    def test_A_one(self):
+        res = main.Aformula(1)
+        self.assertEqual([None], res)
+
+    def test_A_negative(self):
+        res = main.Aformula(-434.222)
+        self.assertEqual([None], res)
+
+
+class FormulaTestsB(unittest.TestCase):
+
+    def test_B(self):
+        res = main.Bformula([2.2, 1.2], 4, 2.12)
+        self.assertEqual([18.921221366927345, 303.9419890192428], res)
+
+    def test_B_zero(self):
+        res = main.Bformula([0])
+        self.assertEqual([None], res)
+
+    def test_B_one(self):
+        res = main.Bformula([1])
+        self.assertEqual([None], res)
+
+    def test_B_negative(self):
+        res = main.Bformula([-434.222])
+        self.assertEqual([None], res)
+
+
 if __name__ == '__main__':
     unittest.main()
