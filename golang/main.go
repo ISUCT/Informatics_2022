6db15e477e55e4d5c721e27c95244d92a8b5c585
AlyonaUi <d4nilovaaliona@yandex.ru>
2022-11-24 18:50:31 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -6,19 +6,24 @@ import (
 )
 
 func main() {
-	par(1.2, 0.48, 2.72)
-}
-
-func par(a, b, e float64) {
-	for x := 0.7; x <= 2.2; x += 0.3 {
-		var y = (math.Pow(b, 3) + math.Pow(math.Sin(a*x), 2)) / (math.Acos(x*b*x) + (math.Pow(e, -x/2)))
-		fmt.Println(y)
+	for x := 0.7; x <= 2.2; x += 0.3{
+	    y := par(1.2, 0.48, 2.72, x)
+	    fmt.Println(y)
 	}
 	var users = [5]float64{0.25, 0.36, 0.56, 0.94, 1.28}
 	for i := 0; i < len(users); i++ {
 		x := users[i]
-		var y float64 = (math.Pow(b, 3) + math.Pow(math.Sin(a*x), 2)) / (math.Acos(x*b*x) + (math.Pow(e, -x/2)))
+		y := par2(1.2, 0.48, 2.72, x)
 		fmt.Println(y)
 	}
+}
+
+func par(a, b, e, x float64) float64 {
+	y := (math.Pow(b, 3) + math.Pow(math.Sin(a*x), 2)) / (math.Acos(x*b*x) + (math.Pow(e, -x/2)))
+	return y		
+}
+func par2(a, b, e, x float64) float64 {
+	var y float64 = (math.Pow(b, 3) + math.Pow(math.Sin(a*x), 2)) / (math.Acos(x*b*x) + (math.Pow(e, -x/2)))
+    return y
 }
 
