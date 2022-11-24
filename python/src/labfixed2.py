4047f49e133f1cd4022f8fe3fa11e8f559342664
Хализова Анна Романовна <117760288+HalizovaAnna@users.noreply.github.com>
2022-11-24 18:45:07 +0300

    fixed(def)

---
+++ python/src/labfixed2.py
@@ -0,0 +1,26 @@
+from numpy import arange
+
+
+# A
+
+def y(x):
+    y = ((((x - a) ** 2) ** (1.0 / 3)) + (abs(x + b) ** (1.0 / 5))) / ((x ** 2 - (a + b) ** 2) ** (1.0 / 9))
+    return y
+
+
+for x in arange(1.23, 7.23, 1.2):
+    x = 1.23
+while x < 7.23:
+    a = 0.8
+    b = 0.4
+    print(y(x))
+    x += 1.2
+
+# B
+xrange = [1.88, 2.26, 3.84, 4.55, -6.21]
+xlen = len(xrange)
+for i in range(0, xlen):
+    x = xrange[i]
+    a = 0.8
+    b = 0.4
+    print(y(x))
