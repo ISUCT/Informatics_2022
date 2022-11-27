c405353cab0c646fdda252a1f34e7c1778644d8f
Rewqaf41 <danyburdin@gmail.com>
2022-11-27 17:36:33 +0300

    update lab.py(add (a, b) in function)

--- python/src/lab.py
+++ python/src/lab.py
@@ -1,13 +1,11 @@
 # 3var
 import math
-a = 2.0
-b = 0.95
 x = 1.25
 print('Задание А:')
 # Задание А
 
 
-def y(x):
+def y(x, a=2.0, b=0.95):
     y = (1 + math.pow(math.log10(x / a), 2)) / (b - math.exp(x / a))
     return y
 
@@ -16,11 +14,10 @@ if __name__ == '__main__':
     while x < 2.75:
         print(y(x))
         x += 0.3
-
     # Задание B
     print('Задание B:')
     xlist = [2.2, 3.78, 4.51, 6.58, 1.2]
     xlen = len(xlist)
     for i in range(0, xlen):
-        x = xlist[i]
         print(y(x))
+        x = xlist[i]
