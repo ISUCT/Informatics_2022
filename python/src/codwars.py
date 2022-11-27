0e921b469458f5a17d715b970e154ddc550367a0
Mewsha <adam.corvum@yandex.ru>
2022-11-27 16:58:34 +0300

    codewars

---
+++ python/src/codwars.py
@@ -0,0 +1,32 @@
+def even_or_odd(number: int):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+
+def count_sheeps(sheep):
+    sheep == list
+    return sheep.count(True)
+
+
+monkey = int()
+def monkey_count(monkey):
+    c = list()
+    for i in range(1, monkey + 1):
+        c.append(i)
+    return c
+
+
+def paperwork(n: int, m: int):
+    if n <= 0 or m <= 0:
+        return 0
+    else:
+        return (m * n)
+
+
+def hero(bullets: int, dragons: int):
+    if (bullets // dragons >= 2):
+        return True
+    else:
+        return False
