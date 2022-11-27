89278018efc8c680322eda6e3c365be0f5c523bf
sergeykochiev <sergeyivanovo547@gmail.com>
2022-11-27 13:32:41 +0300

    codewars done

--- python/src/main.py
+++ python/src/main.py
@@ -4,7 +4,7 @@ import math
 def summ(a: int, b: int) -> int:
     return a + b
 
-
+# lab 1
 def function1(t):
     return math.asin(t ** 2) + math.acos(t ** 3)
 
@@ -23,6 +23,43 @@ def task2(xlist):
         answer.append(function1(n))
     return answer
 
+# codewars
+def even_or_odd(number):
+    if abs(number) % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+    
+
+def count_sheeps(sheep):
+    count = 0
+    for n in range(len(sheep)):
+        if sheep[n] == True:
+            count += 1
+    return count
+
+
+def monkey_count(n):
+    answer = []
+    while n != 0:
+        answer.append(n)
+        n -= 1
+    return answer[::-1]
+
+
+def paperwork(n, m):
+    if n < 0 or m < 0:
+        return 0
+    else:
+        return n * m
+
+
+def hero(bullets, dragons):
+    if 2 * dragons <= bullets:
+        return True
+    else:
+        return False
+    
 
 if __name__ == "__main__":
     print("Hello world")
