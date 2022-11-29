4345629591f880ced33562f62e4cd010b92a6cdc
ZinixZay <romadesl21@gmail.com>
2022-11-29 23:26:41 +0300

    исправлены недочеты

--- golang/main.go
+++ golang/main.go
@@ -6,24 +6,27 @@ import (
 )
 
 func main() {
-	const A, B = 2.5, 4.6
+	const (
+		A float64 = 2.5
+		B float64 = 4.6
+	)
 	fmt.Println("Вывод задачи А по формуле 18:")
-	task_a(A, B)
+	task_a(A, B, 1.1, 3.6, 0.5)
 	fmt.Println("Вывод задачи B по формуле 18:")
-	task_b(A, B)
+	variables := [5]float64{1.2, 1.28, 1.36, 1.46, 2.35}
+	task_b(A, B, variables[:])
 }
 
-func task_a(A, B float64) {
-	for x := 1.1; x <= 3.6; x += 0.5 {
-		answer := formula18(A, B, x)
+func task_a(a, b, xn, xk, dx float64) {
+	for x := xn; x <= xk; x += dx {
+		answer := formula18(a, b, x)
 		fmt.Println(answer)
 	}
 }
 
-func task_b(A, B float64) {
-	variables := [5]float64{1.2, 1.28, 1.36, 1.46, 2.35}
-	for _, x := range variables {
-		answer := formula18(A, B, x)
+func task_b(a, b float64, xs []float64) {
+	for _, x := range xs {
+		answer := formula18(a, b, x)
 		fmt.Println(answer)
 	}
 }
