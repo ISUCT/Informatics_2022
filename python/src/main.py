d45afb2db7f2e77103f6b36a3795279709778eba
DariShantalova <118109238+DariShantalova@users.noreply.github.com>
2022-12-01 23:00:30 +0300

    Update main.py

--- python/src/main.py
+++ python/src/main.py
@@ -9,14 +9,26 @@ if __name__ == "__main__":
 
 
 
-x = 0.11
-while 0.11<= x <=0.36:
-    y= ((math.sin(x) ** 3) + (math.cos(x) ** 3)) * math.log10(x)
-    x += 0.05
-    print('A)', y)
 
-n= [0.2, 0.3, 0.38, 0.43, 0.57]
-for i in range(len(n)):
-    x=n[i]
-    y=((math.sin(x) ** 3) + (math.cos(x) ** 3)) * math.log10(x)
-    print('B)', y)
+def taskA(x_start = 0.11,x_end = 0.36,delta_x = 0.05,x = 0.11):
+    answer: list = []
+    while x_start <= x <= x_end:
+        y = ((math.sin(x) ** 3) + (math.cos(x) ** 3)) * math.log10(x)
+        answer.append(y)
+        x += delta_x
+
+    return answer
+
+    
+
+def taskB(arguments: list = [0.2, 0.3, 0.38, 0.43, 0.57]):
+    answer: list = []
+    for i in range(len(arguments)):
+     x = arguments[i]
+     y = ((math.sin(x) ** 3) + (math.cos(x) ** 3)) * math.log10(x)
+     answer.append(y)
+    return answer
+
+print(taskA())
+print(taskB())
+    
