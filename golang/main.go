42434ed914d318993a8d22daa73f20e8728306cd
SadovnichiyDenis <114995964+SadovnichiyDenis@users.noreply.github.com>
2022-12-02 22:16:04 +0300

    исправления и удаление DS_Dtore
    Стоит подучить функции, потому что многое не понимаю как работает.
    добавил начальную, конечную и дельта х, вставил в функцию.

--- golang/main.go
+++ golang/main.go
@@ -7,8 +7,10 @@ import (
 
 func main() {
 	var masB = []float64{1.16, 1.32, 1.47, 1.65, 1, 93}
-	var masA = []float64{1.2, 1.8, 2.4, 3, 3.6, 4.2}
 	const a float64 = 2.0
+
+	var masA []float64
+
 	fmt.Println("___Задание А___")
 	fmt.Println(TaskA(a, masA))
 	fmt.Println("___Задание В___")
@@ -17,16 +19,20 @@ func main() {
 
 func TaskA(a float64, masA []float64) []float64 {
 	var taskA []float64
-	for x := 0; x < len(masA); x++ {
-		taskA = append(taskA, formula(masA[x], a))
+	var nx float64 = 1.2
+	var kx float64 = 4.2
+	var dx float64 = 0.6
+
+	for x := nx; x <= kx; x += dx {
+		taskA = append(taskA, formula(x, a))
 	}
 	return taskA
 }
 
-func TaskB(a float64, masB []float64) []float64 {
+func TaskB(b float64, masB []float64) []float64 {
 	var taskB []float64
 	for i := 0; i < len(masB); i++ {
-		taskB = append(taskB, formula(masB[i], a))
+		taskB = append(taskB, formula(masB[i], b))
 	}
 	return taskB
 }
