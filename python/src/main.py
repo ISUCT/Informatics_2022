dac6f410932518146ec1a0cf5b8c422fc5aff3a6
DariShantalova <118109238+DariShantalova@users.noreply.github.com>
2022-12-04 23:47:55 +0300

    update main.py

--- python/src/main.py
+++ python/src/main.py
@@ -1,4 +1,6 @@
 import math
+
+
 def summ(a: int, b: int) -> int:
     return a + b
 
@@ -8,9 +10,7 @@ if __name__ == "__main__":
     print(summ(3, 4))
 
 
-
-
-def taskA(x_start = 0.11,x_end = 0.36,delta_x = 0.05,x = 0.11):
+def taskA(x_start=0.11, x_end=0.36, delta_x=0.05, x=0.11):
     answer: list = []
     while x_start <= x <= x_end:
         y = ((math.sin(x) ** 3) + (math.cos(x) ** 3)) * math.log10(x)
@@ -19,16 +19,11 @@ def taskA(x_start = 0.11,x_end = 0.36,delta_x = 0.05,x = 0.11):
 
     return answer
 
-    
 
 def taskB(arguments: list = [0.2, 0.3, 0.38, 0.43, 0.57]):
     answer: list = []
     for i in range(len(arguments)):
-     x = arguments[i]
-     y = ((math.sin(x) ** 3) + (math.cos(x) ** 3)) * math.log10(x)
-     answer.append(y)
+        x = arguments[i]
+        y = ((math.sin(x) ** 3) + (math.cos(x) ** 3)) * math.log10(x)
+        answer.append(y)
     return answer
-
-print(taskA())
-print(taskB())
-    
