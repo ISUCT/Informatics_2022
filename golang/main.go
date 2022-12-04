716f900255892791a46d0edd3f36d18d5cffd2e1
AlyonaUi <d4nilovaaliona@yandex.ru>
2022-12-04 17:25:50 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -10,21 +10,25 @@ func main() {
 		a float64 = 1.2
 		b float64 = 0.48
 	)
-	taskA(a, b, 0.7, 2.2, 0.3)
+	fmt.Println(taskA(a, b, 0.7, 2.2, 0.3))
 	var users = [5]float64{0.25, 0.36, 0.56, 0.94, 1.28}
-	taskB(a, b, users[:])
+	fmt.Println(taskB(a, b, users[:]))
 }
 
-func taskA(a, b, xn, xk, dx float64) {
+func taskA(a, b, xn, xk, dx float64) []float64 {
+	var ansA []float64
     for x := xn; x <= xk; x += dx {
-        fmt.Println(par(a, b, x))
+        ansA = append(ansA, par(a, b, x))
 	}
+	return ansA 
 }
 
-func taskB(a, b float64, xm []float64) {
+func taskB(a, b float64, xm []float64) []float64 {
+	var ansB []float64
 	for _, x := range  xm {
-	    fmt.Println(par(a, b, x))
+	    ansB = append(ansB, par(a, b, x))
 	}
+	return ansB 
 }
 
 func par(a, b, x float64) float64 {
