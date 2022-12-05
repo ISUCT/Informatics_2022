a79e1f45eba7fbc2b55593f15606d503445b93a4
SergeyAnisimov <115336230+SeregeyAnisimov@users.noreply.github.com>
2022-12-05 21:45:11 +0300

    Update main.py

--- python/src/main.py
+++ python/src/main.py
@@ -11,12 +11,14 @@ if __name__ == "__main__":
 
 
 def Calculation(x: float, a: float, b: float):
-    return ((a * x + b) ** (1 / 3)) / ((math.log(x, 10))**2)
+    if x > 0 and x != 1:
+        return ((a * x + b) ** (1 / 3)) / ((math.log(x, 10))**2)
+    return None
 
 
 def Aformula(x=2.1, a=1.35, b=0.98, x_start=1.14, x_finish=4.24, x_delta=0.62):
     list_reply: list = []
-    if x_start > x or x > x_finish or x < 0 or x == 1:
+    if x_start > x or x > x_finish:
         list_reply.append(None)
     while x_start <= x <= x_finish:
         x += x_delta
@@ -29,9 +31,6 @@ def Bformula(variables=[0.35, 1.28, 3.51, 5.21, 4.16], a=1.35, b=0.98):
     list_reply: list = []
     for i in range(len(variables)):
         x = variables[i]
-        if x != 1 and x > 0:
-            y = Calculation(x, a, b)
-            list_reply.append(y)
-        else:
-            list_reply.append(None)
+        y = Calculation(x, a, b)
+        list_reply.append(y)
     return list_reply
