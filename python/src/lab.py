866e8b0d00a50097e188122774ce33eba8fb6255
tenacity8 <nastyss356@gmail.com>
2022-12-05 23:42:54 +0300

    tasks1

--- python/src/lab.py
+++ python/src/lab.py
@@ -1,12 +1,11 @@
 import math
 
-a = 0.1
-b = 0.5
-
-
-def y(x):
+def y(x): 
+    a = 0.1
+    b = 0.5
     y=(a + (math.tan(b * x)) ** 2) / (b + (1/math.tan(a * x)) ** 2)
     return y
+
 
 #  task №17 A
 print("task №17 A")
@@ -20,11 +19,12 @@ while x < x_last:
     print(f"x = {x} y = {y(x)}")
     x += step
 
+
 #  task №17 B
 print("task №17 B")
 
 array_x = [0.2, 0.3, 0.44, 0.6, 0.56]
 for x in array_x:
     print(f"x = {x} y = {y(x)}")
-    
+
     
