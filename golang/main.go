4863fc715c2b9b894675bd638a71f668bca51e00
Дмитрий Лобанов <lobzikdl2004@gmail.com>
2022-12-05 09:01:38 +0300

    fixed slices` problems and deleted func form

--- golang/main.go
+++ golang/main.go
@@ -6,26 +6,23 @@ import (
 )
 
 func main() {
-	fmt.Println(zadA())
-	fmt.Println(zadB())
-}
-
-func zadA() []float64 {
 	fmt.Println("ZAD A")
-	var i float64 = 1.25
+	fmt.Println(zadA(1.25, 3.25, 0.4))
+	fmt.Println("ZAD B")
+	fmt.Println(zadB([]float64{1.84, 2.71, 3.81, 4.56, 5.62}))
+}
+func zadA(xn float64, xk float64, dx float64) []float64 {
+	var x float64 = xn
 	var massivA = make([]float64, 6)
-	var count int = 0
-	for ; i <= 3.25; i += 0.4 {
-		massivA[count] = form(i)
-		count += 1
+	for ; x <= xk; x += dx {
+		massivA = append(massivA, math.Sqrt(math.Sqrt(math.Abs(x*x-2.5)))+math.Cbrt(math.Log10(x*x)))
 	}
 	return massivA
 }
-func zadB() []float64 {
-	fmt.Println("ZAD B")
-	var massivB = []float64{form(1.84), form(2.71), form(3.81), form(4.56), form(5.62)}
+func zadB(massivB []float64) []float64 {
+	var i int8 = 0
+	for ; i <= 4; i += 1 {
+		massivB[i] = math.Sqrt(math.Sqrt(math.Abs(massivB[i]*massivB[i]-2.5))) + math.Cbrt(math.Log10(massivB[i]*massivB[i]))
+	}
 	return massivB
-}
-func form(x float64) float64 {
-	return (math.Sqrt(math.Sqrt(math.Abs(x*x-2.5))) + math.Cbrt(math.Log10(x*x)))
 }
