0ad986c948d0c641ee922fc8d0ef11cac083ca63
Demyanenko_Matvey <fut4ded232@yandex.ru>
2022-12-07 01:02:00 +0300

    labcodewar(1-8)

--- python/src/main.py
+++ python/src/main.py
@@ -1,3 +1,4 @@
+from codewar import *
 import math
 
 a = 4.1
@@ -28,3 +29,18 @@ print("task №23 B")
 array_x = [1.9, 2.15, 2.34, 2.74, 3.16]
 for x in array_x:
     print(f"y({x}) = {Y(x)}")
+
+
+print(even_or_odd(2))
+print(count_sheeps([True, True, True, False,
+                    True, True, True, True,
+                    True, False, True, False,
+                    True, False, False, True,
+                    True, True, True, True,
+                    False, False, True, True]))
+print(monkey_count(5))
+print(paperwork(10, 10))
+print(hero(20, 5))
+print(correct_polish_letters("Jędrzej Błądziński"))
+print(sum_of_minimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]))
+print(find_all([6, 9, 3, 4, 3, 82, 11], 3))
