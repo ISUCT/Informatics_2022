23379ac7dc676adf04bb801f831622686e60d8bb
DESKTOP-R7LKOP3\lebed <lebedevegor1701@yandex.ru>
2022-12-06 09:16:27 +0300

    rewrote first task code from python to go language

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	taskA(2, 3, 0.11)
+	taskB(2, 3)
+}
+
+func task(x, a, b float64) float64 {
+	return (math.Asin(math.Pow(x, a)) + math.Acos(math.Pow(x, b)))
+}
+
+func taskA(a, b, x float64) {
+	fmt.Println(`Part A`)
+	for x <= 0.36 {
+		fmt.Println(task(x, a, b))
+		x += 0.05
+	}
+}
+
+func taskB(a, b float64) {
+	fmt.Println(`Part B`)
+	fmt.Println(task(0.08, a, b))
+	fmt.Println(task(0.26, a, b))
+	fmt.Println(task(0.35, a, b))
+	fmt.Println(task(0.41, a, b))
+	fmt.Println(task(0.53, a, b))
 }
