694c3e2dbf60fc41e3e429149ed0ccedc67f919e
Mewsha <adam.corvum@yandex.ru>
2022-12-07 18:57:44 +0300

    add 3 last zadaniya

--- python/src/codwars.py
+++ python/src/codwars.py
@@ -31,8 +31,37 @@ def hero(bullets: int, dragons: int):
         return False
 
 
+def correct_polish_letters(st):
+    st = st.translate(str.maketrans('ąćęłńóśźż', 'acelnoszz'))
+    return (st)
+
+
+def correct_polish_letters(st):
+    l = "ąćęłńóśźż"
+    lt = "acelnoszz"
+    for i in range(len(l)):
+        st = st.replace(l[i], lt[i])
+    return st
+
+
+def find_all(array: list, n: int):
+    c = []
+    for i in range(len(array)):
+        if array[i] == n:
+            c.append(i)
+    return c
+
+
+def sum_of_minimums(numbers):
+    c = sum(min(x) for x in numbers)
+    return c
+
+
 print('1)', even_or_odd(4))
-print('2)',count_sheeps([True, True, True, False, True, True, True, True]))
-print('3)',monkey_count(10))
-print('4)',paperwork(10, 3))
-print('5)',hero(10, 3))
+print('2)', count_sheeps([True, True, True, False, True, True, True, True]))
+print('3)', monkey_count(10))
+print('4)', paperwork(10, 3))
+print('5)', hero(10, 3))
+print('6)', correct_polish_letters("Lech Wałęsa"))
+print('7)', find_all([6, 9, 3, 4, 3, 82, 11], 3))
+print('8)', sum_of_minimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]))
