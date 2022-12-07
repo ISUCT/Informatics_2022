2a02f721862c19510a1377962534cb554a09824d
sergeykochiev <sergeyivanovo547@gmail.com>
2022-12-07 11:44:44 +0300

    new codewars challenges

--- python/src/main.py
+++ python/src/main.py
@@ -61,6 +61,29 @@ def hero(bullets, dragons):
         return True
     else:
         return False
+    
+
+def correct_polish_letters(st):
+    polish = 'ąćęłńóśźż'
+    normal = 'acelnoszz'
+    for n in range(len(polish)):
+        st = st.replace(polish[n], normal[n])
+    return st
+
+
+def find_all(array, n):
+    answer = []
+    for x in range(len(array)):
+        if array[x] == n:
+            answer.append(x)
+    return answer
+
+
+def sum_of_minimums(numbers):
+    answer = 0
+    for n in numbers:
+        answer += min(n)
+    return answer
 
 
 if __name__ == "__main__":
