f9112573645fac57977d47747601268ca0a722ef
ZinixZay <romadesl21@gmail.com>
2022-12-07 20:22:32 +0300

    changes

--- golang/main.go
+++ golang/main.go
@@ -15,6 +15,6 @@ func main() {
 	fmt.Println("School:", internal.School(5, 5))
 	fmt.Println("Hero:", internal.LuckyHero(9, 4))
 	fmt.Println("Polish messages:", internal.PolToEng("Jędrzej Błądziński"))
-	fmt.Println("7:", internal.FindAll([]int(6, 9, 3, 4, 3, 82, 4), 3))
-	fmt.Println("8:", internal.OnlyMin([][]int{{1, 2, 3, 4, 5}, {5, 6, 7, 8, 9}, {20, 21, 35, 56, 100}}))
+	// fmt.Println("7:", internal.FindAll([]int(6, 9, 3, 4, 3, 82, 4), 3))
+	// fmt.Println("8:", internal.OnlyMin([][]int{{1, 2, 3, 4, 5}, {5, 6, 7, 8, 9}, {20, 21, 35, 56, 100}}))
 }
