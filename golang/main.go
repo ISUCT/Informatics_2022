af67f1beada082ed70ce943890a4526531ebd2da
Artem Barannikoff <asdfghq000@gmail.com>
2022-12-08 19:39:30 +0300

    exercised in packaging

--- golang/main.go
+++ golang/main.go
@@ -11,4 +11,6 @@ func main() {
 	fmt.Println(internal.ZadA(1.25, 3.25, 0.4))
 	fmt.Println("ZADB")
 	fmt.Println(internal.ZadB([]float64{1.84, 2.71, 3.81, 4.56, 5.62}))
+	fmt.Println("")
+	internal.Vike()
 }
