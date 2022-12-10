bf308b1c7b812cf973cb2f6ef95051c2240f1c14
DragIvBor <yvan.borzov@yandex.ru>
2022-12-10 14:21:59 +0300

    update lab_2.py

--- python/src/lab_2.py
+++ python/src/lab_2.py
@@ -1,25 +1,33 @@
 import math
-a = 1.35
-b = 0.98
+
 x = 1.14
 print('Задание А:')
+
+
 # Задание А
 
-
-def y(x):
+def A(x, a=1.35, b=0.98):
     y = (((a * x + b) ** (1 / 3)) / ((math.log(x, math.e)) ** 2))
     return y
 
 
 if __name__ == '__main__':
     while x < 4.24:
-        print(y(x))
+        print(A(x))
         x += 0.62
 
-    # Задание B
-    print('Задание B:')
-    xlist = [0.35, 1.28, 3.51, 5.21, 4.16]
-    xlen = len(xlist)
-    for i in range(0, xlen):
-        x = xlist[i]
-        print(y(x))
+
+# Задание B
+
+
+def B(x):
+    y = (((1.35 * x + 0.98) ** (1 / 3)) / ((math.log(x, math.e)) ** 2))
+    return y
+
+
+print('Задание B:')
+xlist = [0.35, 1.28, 3.51, 5.21, 4.16]
+xlen = len(xlist)
+for i in range(0, xlen):
+    x = xlist[i]
+    print(B(x))
