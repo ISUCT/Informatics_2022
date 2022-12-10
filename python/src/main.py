121142df565df3f38d71c4536019b738cc2f1153
StupachenkoArtem <119942880+StupachenkoArtem@users.noreply.github.com>
2022-12-10 13:13:41 +0300

    Update main.py

--- python/src/main.py
+++ python/src/main.py
@@ -6,49 +6,47 @@ if __name__ == "__main__":
     print("Hello world")
     print(summ(3, 4))
 
-def testA(x_start, x_finish, x_delta, x, a, b):
-    
-    # 1 zadanie
-    def EvenOrOdd(n):
-        if n % 2 == 0:
-            return 'Even'
-    return 'Odd'
 
-    # 2 zadanie
+def EvenOrOdd(number):
+    x = number
+    if x % 2 == 0:
+        return 'Even'
+    else:
+        return 'Odd'
+
+
 def count_sheeps(sheep):
     a = 0
     for i in sheep:
-        if i == True:
+        if i is True:
             a += 1
 
-    # 3 zadanie
+
 def monkey_count(n):
-    return [x for x in range(1, n+1)]
+    return [x for x in range(1, n + 1)]
 
-#4 zadanie
+
 def paperwork(n, m):
-    return n * m if n > 0 and m > 0 else 0
+    if n > 0 and m > 0:
+        return n * m
+    return 0
 
-#5 zadanie
-    bulletsNeeded = dragons * 2;
-    if bullets >= bulletsNeeded:
-        return True;
-    else:
-        return False;   
 
-#6 zadanie
+def hero(bullets, dragons):
+    return bullets // dragons >= 2 if dragons != 0 else True
+
+
 def correct_polish_letters(st):
     return st.translate(str.maketrans("ąćęłńóśźż", "acelnoszz"))
 
-#7 zadanie
+
 def find_all(array, n):
     res = []
     for i in range(len(array)):
         if array[i] == n:
-            res.append(i)   
+            res.append(i)
     return res
 
-#8 zadanie
+
 def sum_of_minimums(numbers):
     return sum([min(x) for x in numbers])
-    return a
