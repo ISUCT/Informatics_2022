b1e3abf0c2af4bb020a64185c4014bf703f014bb
Ivan Gusev <ivan.gusev04@mail.ru>
2022-12-18 19:07:27 +0300

    теперь функция numA возвращает значения

--- golang/main.go
+++ golang/main.go
@@ -9,7 +9,10 @@ func main() {
 	fmt.Println("Hello world")
 
 	fmt.Println("Num A")
-	numA(0.4, 0.8, 3.2, 6.2, 0.6)
+	numAResults := numA(0.4, 0.8, 3.2, 6.2, 0.6)
+	for i := 0; i < len(numAResults); i++ {
+		fmt.Println(numAResults[i])
+	}
 	fmt.Println("Mass")
 	digit := [5]float64{4.48, 3.56, 2.78, 5.28, 3.21}
 	Mass(0.4, 0.8, digit)
@@ -18,10 +21,13 @@ func main() {
 func lab(a float64, b float64, x float64) float64 {
 	return (((math.Pow(a, x) - math.Pow(b, x)) / math.Log10(a/b)) * math.Pow((a*b), 1.0/3.0))
 }
-func numA(a float64, b float64, xn float64, xk float64, xd float64) {
+func numA(a float64, b float64, xn float64, xk float64, xd float64) []float64 {
+	var array []float64
 	for x := xn; x < xk; x += xd {
-		fmt.Println(lab(a, b, x))
+
+		array = append(array, lab(a, b, x))
 	}
+	return array
 }
 func Mass(a float64, b float64, digit [5]float64) {
 	for i := 0; i < len(digit); i++ {
