e00829842073c742ba571c9ca81f0bed5e305830
Artem Barannikoff <asdfghq000@gmail.com>
2022-12-20 21:02:53 +0300

    added solution w/o package "strings" for polish

--- golang/internal/codewars.go
+++ golang/internal/codewars.go
@@ -36,6 +36,13 @@ func SchoolPaperwork(n int, m int) int {
 		return 0
 	}
 }
+func SchoolPaperwork1(n int, m int) int {
+	if (n < 0) || (m < 0) {
+		return 0
+	} else {
+		return m * n
+	}
+}
 func IsHe(bullets int, dragons int) string {
 	if bullets >= dragons*2 {
 		return "True"
@@ -45,7 +52,7 @@ func IsHe(bullets int, dragons int) string {
 }
 
 // ПОЛЬСКАЯ КОРОВА ЗАХОТЕЛА
-func Polish(text string) string {
+func PolishA(text string) string {
 	var changing [][]string = [][]string{
 		{"ą", "a"},
 		{"ć", "c"},
@@ -61,6 +68,25 @@ func Polish(text string) string {
 		text = strings.Replace(text, changing[i][0], changing[i][1], 1)
 	}
 	return text
+}
+func PolishB(text string) string { //БЕЗ ПАКЕТА STRINGS
+	var changingA string = "ąćęłńóśźż"
+	var changingB string = "acelnoszz"
+	conv := []byte(text)
+	var count int8
+	for i := 0; i < len(conv); i++ {
+		for k := 0; k < len(changingA); k++ {
+			count++
+			j := k / 2
+			if (count % 2) == 0 {
+				if conv[i] == changingA[k] {
+					conv[i] = changingB[j]
+					conv = append(conv[:i-1], conv[i:]...)
+				}
+			}
+		}
+	}
+	return string(conv)
 }
 func FindAll(arrFa []int, n int) []int {
 	var res []int
--- golang/main.go
+++ golang/main.go
@@ -10,9 +10,10 @@ func main() {
 	fmt.Println(internal.EvenOrOdd(1))
 	fmt.Println(internal.CountingSheeps([]bool{true, false, true, true, true}))
 	fmt.Println(internal.CountingMonkeys(10))
-	fmt.Println(internal.SchoolPaperwork(-2, 5))
+	fmt.Println(internal.SchoolPaperwork1(-2, 5))
 	fmt.Println(internal.IsHe(5, 3))
-	fmt.Println(internal.Polish("Jędrzej Błądziński")) //ПОЛЬСКА НЕ РОБИТ
+	fmt.Println(internal.PolishA("Polska krowa ze śmierdzącymi stringami"))
+	fmt.Println(internal.PolishB("Polska krowa bez śmierdzących stringów"))
 	fmt.Println(internal.FindAll([]int{1, 2, 1, 2, 9, 3, 5, 1}, 1))
 	var exampleForSumOfMin [][]int = [][]int{
 		{1, 2, 3, 4, 5},
