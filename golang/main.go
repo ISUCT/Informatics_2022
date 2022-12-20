cea712c372740af164a159f55f4ee885fcf8a73b
Alexey Arkhipov <chikibrikic@yandex.ru>
2022-12-20 19:52:29 +0300

    Лабораторная работа #3 вар.2 и все задачи CodeWars

--- golang/main.go
+++ golang/main.go
@@ -3,11 +3,26 @@ package main
 import (
 	"fmt"
 	"math"
+
+	"isuct.ru/informatics2022/internal"
 )
 
 func main() {
-	ZadanieA(1.35, 0.98, 4.24, 1.14)
-	ZadanieB(1.35, 0.98, 0)
+	//ZadanieA(1.35, 0.98, 4.24, 1.14)
+	//ZadanieB(1.35, 0.98, 0)
+	internal.EvenOrOdd(4)
+	internal.CountingSheep([]bool{true, true, true, false,
+		true, true, true, true,
+		true, false, true, false,
+		true, false, false, true,
+		true, true, true, true,
+		false, false, true, true})
+	internal.MonkeysCount(4)
+	fmt.Println(internal.PaperworkCount(0, -1))
+	fmt.Println(internal.HeroWithGunShootsDragons(2, 6))
+	fmt.Println(internal.PolishCow("Grzegorz Brzęczyszczykiewicz"))
+	fmt.Println(internal.FindAll([]int{1, 4, 5, 7, 2, 1, 4, 8, 8}, 4))
+	fmt.Println(internal.SumOfMin([][]int{{1, 2, 3, 4, 5}, {5, 6, 7, 8, 9}, {20, 21, 34, 56, 100}}, 3, 5))
 
 }
 
@@ -26,3 +41,5 @@ func ZadanieB(a float64, b float64, c int64) {
 		fmt.Println(y)
 	}
 }
+
+// https://youtu.be/diioRPYBL9k
