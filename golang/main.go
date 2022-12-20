1ba08f5750426c48bdc2b8d7beaae89ea13a5aca
unknown <ssmrll@yandex.ru>
2022-12-20 12:27:44 +0300

    1st part of codewars task

--- golang/main.go
+++ golang/main.go
@@ -1,26 +1,65 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Задание А")
-	for x := 1.23; x <= 7.23; x += 1.2 {
-		var ch = (math.Pow(math.Pow(x-0.8, 2.0), 1/3.0) + (math.Pow(math.Abs(x+0.4), 1/5.0)))
-		var zn = (math.Pow(math.Pow(x, 2.0)-math.Pow(0.8+0.4, 2.0), 1/9.0))
-		var y = (ch / zn)
-		fmt.Println(y)
+
+}
+
+func evenorodd(s int) {
+	fmt.Println("1st task from codewars")
+	if s%2 == 0 {
+		fmt.Println("четное")
+	} else {
+		fmt.Println("нечетное")
+	}
+}
+
+func countingsheeps(numbers []bool) int {
+	fmt.Println("2nd task from codewars")
+	var count = 0
+
+	for _, i := range numbers {
+		// просчитывание массива, используем пустую переменную, чтобы не было ошибки but not used
+		if i {
+			count += 1
+		}
+
+	}
+	return (count)
+}
+
+func monkey(n int) []int {
+	fmt.Println("3d task from codewars")
+	monc := make([]int, n)
+	// команда make позволяет вывести массив из n переменных типа int с пустым значением
+	for i := range monc {
+		//диапозон monc
+		monc[i] = i + 1
+
 	}
 
-	fmt.Println("Задание B")
-	mass_B := []float64{1.9, 2.15, 2.34, 2.73, 3.16}
-	for _, x := range mass_B {
-		var ch = (math.Pow(math.Pow(x-0.8, 2.0), 1/3.0) + (math.Pow(math.Abs(x+0.4), 1/5.0)))
-		var zn = (math.Pow(math.Pow(x, 2.0)-math.Pow(0.8+0.4, 2.0), 1/9.0))
-		var y = (ch / zn)
-		fmt.Println(y)
+	return (monc)
+}
 
+func classmates(n, m int) {
+	fmt.Println("4th task from codewars")
+	if n < 0 || m < 0 {
+		fmt.Println(0)
+	} else {
+		fmt.Println(n * m)
+	}
+	//если я буду использовать return будет ошибка too many return values
+
+}
+
+//у нас есть какое-то кол-во драконов x, и какое-то количество пуль y. если мы возьмем у/2<x, то умрем
+
+func dragons(x, y int) {
+	fmt.Println("5th task from codewars")
+	if y/2 < x {
+		fmt.Println("false")
+	} else {
+		fmt.Println("true")
 	}
 }
